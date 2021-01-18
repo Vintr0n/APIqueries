6e856a7b9bbfdfd4056232623b4c11c006095582
Vintr0n <49721167+Vintr0n@users.noreply.github.com>
2021-01-18 14:25:06 +0000

    Update SQL API.sql

--- SQL API.sql
+++ SQL API.sql
@@ -5,6 +5,8 @@ DECLARE @Response TABLE (txt NVARCHAR(MAX));
 DECLARE @ExtractDateTime AS DATETIME
 SET @ExtractDateTime = GETDATE()
 
+-- Found that setting an IP address is required for those behind a proxy server
+EXEC sp_OAMethod @Object, 'setProxy', NULL, '2', '127.0.0.1:8080'
 EXEC sp_OACreate 'MSXML2.XMLHTTP', @Object OUT;
 EXEC sp_OAMethod @Object, 'OPEN', NULL, 'GET',
              'https://api.coinbase.com/v2/prices/LINK-GBP/sell', 
@@ -26,7 +28,7 @@ SELECT * FROM @Response
 -- Find unnecessary characters
 --PRINT SUBSTRING(@replace,100564,30);
 
--- Remove unnecessary characters
+-- Remove unnecessary characters 
 DECLARE @replace VARCHAR(MAX)
 SET @replace = REPLACE(REPLACE(REPLACE(REPLACE(@temp,'new Date(',''),')',''),'\''S','S'),'\','')
 
